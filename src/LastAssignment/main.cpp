06bf62d91b10929c09eae1d8f6c10e5554114b94
landcelita <landcelita@akane.waseda.jp>
2022-07-05 02:10:28 +0900

    first commit

---
+++ src/LastAssignment/main.cpp
@@ -0,0 +1,249 @@
+#include <cmath>
+#include <cstdio>   
+#include <vector>
+
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#define GLFW_DLL
+#define GLFW_INCLUDE_GLU  // GLUライブラリを使用するのに必要 / Required to use GLU
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+static int WIN_WIDTH = 500;                      // ウィンドウの幅 / Window width
+static int WIN_HEIGHT = 500;                     // ウィンドウの高さ / Window height
+static const char *WIN_TITLE = "OpenGL Course";  // ウィンドウのタイトル / Window title
+
+// 頂点クラス
+// Vertex class
+struct Vertex {
+    Vertex(const glm::vec3 &position_, const glm::vec3 &color_)
+        : position(position_)
+        , color(color_) {
+    }
+
+    glm::vec3 position;
+    glm::vec3 color;
+};
+
+// clang-format off
+static const glm::vec3 positions[8] = {
+    glm::vec3(-1.0f, -1.0f, -1.0f),
+    glm::vec3( 1.0f, -1.0f, -1.0f),
+    glm::vec3(-1.0f,  1.0f, -1.0f),
+    glm::vec3(-1.0f, -1.0f,  1.0f),
+    glm::vec3( 1.0f,  1.0f, -1.0f),
+    glm::vec3(-1.0f,  1.0f,  1.0f),
+    glm::vec3( 1.0f, -1.0f,  1.0f),
+    glm::vec3( 1.0f,  1.0f,  1.0f)
+};
+
+static const glm::vec3 colors[6] = {
+    glm::vec3(1.0f, 0.0f, 0.0f),  // 赤
+    glm::vec3(0.0f, 1.0f, 0.0f),  // 緑
+    glm::vec3(0.0f, 0.0f, 1.0f),  // 青
+    glm::vec3(1.0f, 1.0f, 0.0f),  // イエロー
+    glm::vec3(0.0f, 1.0f, 1.0f),  // シアン
+    glm::vec3(1.0f, 0.0f, 1.0f),  // マゼンタ
+};
+
+static const unsigned int faces[12][3] = {
+    { 7, 4, 1 }, { 7, 1, 6 },
+    { 2, 4, 7 }, { 2, 7, 5 },
+    { 5, 7, 6 }, { 5, 6, 3 },
+    { 4, 2, 0 }, { 4, 0, 1 },
+    { 3, 6, 1 }, { 3, 1, 0 },
+    { 2, 5, 3 }, { 2, 3, 0 }
+};
+// clang-format on
+
+// バッファを参照する番号
+// Indices for vertex/index buffers
+GLuint vertexBufferId;
+GLuint indexBufferId;
+
+// 立方体の回転角度
+// Rotation angle for animating a cube
+static float theta = 0.0f;
+
+// ユーザ定義のOpenGLの初期化
+// User-define OpenGL initialization
+void initializeGL() {
+    // 深度テストの有効化
+    // Enable depth testing
+    glEnable(GL_DEPTH_TEST);
+
+    // 背景色の設定 (黒)
+    // Background color (black)
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    // 頂点配列の作成
+    // Prepare vertex array
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    int idx = 0;
+    for (int face = 0; face < 6; face++) {
+        for (int i = 0; i < 3; i++) {
+            vertices.push_back(Vertex(positions[faces[face * 2 + 0][i]], colors[face]));
+            indices.push_back(idx++);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            vertices.push_back(Vertex(positions[faces[face * 2 + 1][i]], colors[face]));
+            indices.push_back(idx++);
+        }
+    }
+
+    // 頂点バッファオブジェクトの作成
+    // Create vertex buffer object
+    glGenBuffers(1, &vertexBufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(),
+                 vertices.data(), GL_STATIC_DRAW);
+
+    // 頂点番号バッファオブジェクトの作成
+    // Create index buffer object
+    glGenBuffers(1, &indexBufferId);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(),
+                 indices.data(), GL_STATIC_DRAW);
+}
+
+// ユーザ定義のOpenGL描画
+// User-defined OpenGL drawing
+void paintGL() {
+    // 背景色と深度値のクリア
+    // Clear background color and depth values
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // 座標の変換
+    // Coordinate transformation
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, (float)WIN_WIDTH / (float)WIN_HEIGHT, 0.1f, 1000.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(3.0f, 4.0f, 5.0f,   // 視点の位置 / Eye position
+              0.0f, 0.0f, 0.0f,   // 見ている先 / Looking position
+              0.0f, 1.0f, 0.0f);  // 視界の上方向 / Upward direction
+
+    // 回転行列の設定
+    // Setup rotation matrix
+    glPushMatrix();
+    glRotatef(theta, 0.0f, 1.0f, 0.0f);
+
+    // 頂点バッファの有効化
+    // Enable vertex buffer object
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+
+    // 頂点情報の詳細を設定
+    // Setup details for vertex array
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, sizeof(Vertex), (void *)offsetof(Vertex, position));
+
+    glEnableClientState(GL_COLOR_ARRAY);
+    glColorPointer(3, GL_FLOAT, sizeof(Vertex), (void *)offsetof(Vertex, color));
+
+    // 三角形の描画
+    // Draw triangles
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+    // 頂点バッファの無効化
+    // Disable vertex buffer object
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // 回転行列の破棄
+    // Dispose of rotation matrix
+    glPopMatrix();
+}
+
+// ウィンドウサイズ変更のコールバック関数
+// Callback function for window resizing
+void resizeGL(GLFWwindow *window, int width, int height) {
+    // ユーザ管理のウィンドウサイズを変更
+    // Update user-managed window size
+    WIN_WIDTH = width;
+    WIN_HEIGHT = height;
+
+    // GLFW管理のウィンドウサイズを変更
+    // Update GLFW-managed window size
+    glfwSetWindowSize(window, WIN_WIDTH, WIN_HEIGHT);
+
+    // 実際のウィンドウサイズ (ピクセル数) を取得
+    // Get actual window size by pixels
+    int renderBufferWidth, renderBufferHeight;
+    glfwGetFramebufferSize(window, &renderBufferWidth, &renderBufferHeight);
+
+    // ビューポート変換の更新
+    // Update viewport transform
+    glViewport(0, 0, renderBufferWidth, renderBufferHeight);
+}
+
+// アニメーションのためのアップデート
+// Update parameters for animation
+void animate() {
+    theta += 1.0f;  // 1度だけ回転 / Rotate 1 degree of angle
+}
+
+int main(int argc, char **argv) {
+    // OpenGLを初期化する
+    // OpenGL initialization
+    if (glfwInit() == GLFW_FALSE) {
+        fprintf(stderr, "Initialization failed!\n");
+        return 1;
+    }
+
+    // Windowの作成
+    // Create a window
+    GLFWwindow *window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE,
+                                          NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "Window creation failed!\n");
+        glfwTerminate();
+        return 1;
+    }
+
+    // OpenGLの描画対象にwindowを指定
+    // Specify window as an OpenGL context
+    glfwMakeContextCurrent(window);
+
+    // OpenGL 3.x/4.xの関数をロードする (glfwMakeContextCurrentの後でないといけない)
+    const int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0) {
+        fprintf(stderr, "Failed to load OpenGL 3.x/4.x libraries!\n");
+        return 1;
+    }
+
+    // バージョンを出力する / Check OpenGL version
+    printf("Load OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+    // ウィンドウのリサイズを扱う関数の登録
+    // Register a callback function for window resizing
+    glfwSetWindowSizeCallback(window, resizeGL);
+
+    // ユーザ指定の初期化
+    // User-specified initialization
+    initializeGL();
+
+    // メインループ
+    while (glfwWindowShouldClose(window) == GLFW_FALSE) {
+        // 描画 / Draw
+        paintGL();
+
+        // アニメーション / Animation
+        animate();
+
+        // 描画用バッファの切り替え
+        // Swap drawing target buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // 後処理 / Postprocess
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
