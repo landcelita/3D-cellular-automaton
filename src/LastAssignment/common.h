ffa069874440ba38cf668f7480fd8e49ea4732a8
landcelita <landcelita@akane.waseda.jp>
2022-07-05 02:21:57 +0900

    arcball_controlをベースに作る

--- src/LastAssignment/common.h
+++ src/LastAssignment/common.h
@@ -1,8 +1,8 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-static const char* SOURCE_DIRECTORY = "C:/graphics/src/day3/Program11-1/";
-static const char* SHADER_DIRECTORY = "C:/graphics/src/day3/Program11-1/shaders/";
-static const char* DATA_DIRECTORY = "C:/graphics/src/day3/Program11-1/data/";
+static const char* SOURCE_DIRECTORY = "C:/graphics/src/LastAssignment/";
+static const char* SHADER_DIRECTORY = "C:/graphics/src/LastAssignment/shaders/";
+static const char* DATA_DIRECTORY = "C:/graphics/src/LastAssignment/data/";
 
 #endif  // _COMMON_H_
--- src/LastAssignment/shaders/render.frag
+++ src/LastAssignment/shaders/render.frag
@@ -1,14 +1,12 @@
 #version 330
 
-// Varying変数: 頂点シェーダから受け取った値
-// Varying variables: received from a vertex shader.
+// Varying変数
 in vec3 f_fragColor;
 
-// Varying変数: ディスプレイへの出力変数
-// Varying variables: here for exporting colors to the display 
+// ディスプレイへの出力変数
 out vec4 out_color;
 
 void main() {
-    // 描画色を代入 / Store pixel colors
+    // 描画色を代入
     out_color = vec4(f_fragColor, 1.0);
 }
